924fc83e02d602cb41c2a7e82d075924297dc38e
Alexandra Barnett <alex.barnett@innovationwide.co.uk>
2017-06-16 15:40:57 +0100

    First commit

---
+++ imports/ui/Player.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Players } from './../api/players';
+
+export default class Player extends Component {
+    render() {
+        return (
+            <div key={this.props.player._id} className='item'>
+                <div className='player'>
+                    <div>
+                        <h3 className='player__name'>{this.props.player.name}</h3>
+                        <p className='player__stats'>
+                            {this.props.player.score} points(s).
+                        </p>
+                    </div>
+                    
+                    <div className='player__actions'>
+                        <button className='button button--round' onClick={() => {
+                            Players.update({ _id: this.props.player._id }, { $inc: { score: -1 } })
+                        }}>
+                            -1
+                        </button>
+                        <button className='button button--round' onClick={() => { 
+                            Players.update({ _id: this.props.player._id }, { $inc: { score: 1 } }) 
+                        }}>
+                            +1
+                        </button>
+                        <button className='button button--round' onClick={() => { Players.remove({ _id: this.props.player._id }) }}>
+                            X
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Player.propTypes = {
+    player: PropTypes.object.isRequired
+};
+
